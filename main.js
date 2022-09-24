4a08ed893dd0142ee40fff6b722b89513a3840ea
matheusAlexandreRibeiroRocha <108072264+matheusAlexandreRibeiroRocha@users.noreply.github.com>
2022-09-23 22:27:33 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+menuListArray = ["Pizza Vegetariana","Pizza de calabreza","Pizza quatro queijos","Pizza portuguesa" ];
+    function getMenu(){
+    var htmldata="";
+    menuListArray.sort();
+}
+
+function addItem(){
+    var htmldata;
+    var item=document.getElementById("addItem").value;
+
+
+}
+
+function addTop(){
+//Completar o código
+}
